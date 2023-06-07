a2f7dd7db7b79bdb63c85c877e4429fcea313b4b
itlantu <39409253+itlantu@users.noreply.github.com>
2023-06-07 20:26:44 +0800

    用opencv实现用拉普拉斯算子的方差得到图像的模糊程度

---
+++ src/image_processing/getImageBlurriness.py
@@ -0,0 +1,11 @@
+import cv2
+
+# 用拉普拉斯算子获得指定路径图像的模糊程度
+def getImageBlurriness(img_path: str) -> float:
+    # imread函数默认读取图像的格式是BGR
+    img = cv2.imread(img_path)
+    # BGR转灰度图
+    img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 计算拉普拉斯算子的方差（模糊程度）
+    blurriness = cv2.Laplacian(img2gray, cv2.CV_64F).var()
+    return blurriness
