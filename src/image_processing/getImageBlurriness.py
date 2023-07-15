f186444e720bdd47dc3f749197ac7694b89ba4c8
itlantu <39409253+itlantu@users.noreply.github.com>
2023-07-15 13:53:01 +0800

    拉普拉斯方差计算图像的模糊程度
    添加了图像打开失败抛出异常

--- src/image_processing/getImageBlurriness.py
+++ src/image_processing/getImageBlurriness.py
@@ -1,9 +1,13 @@
 import cv2
 
+
 # 用拉普拉斯算子的方差来计算指定路径图像的模糊程度
 def getImageBlurriness(img_path: str) -> float:
     # imread函数默认读取图像的格式是BGR
     img = cv2.imread(img_path)
+    if img is None:
+        raise Exception("Failed to load image {}".format(filename))
+    
     # BGR转灰度图
     img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # 计算拉普拉斯算子的方差（模糊程度）
